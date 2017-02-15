b0c23c083acc703115a00c2874ca9bff947d4682
ExploHash <jbtwaalf@gmail.com>
2017-02-15 17:21:30 +0100

    trying to debug...

--- daemon.cpp
+++ daemon.cpp
@@ -1,55 +1,80 @@
 // my first program in C++
 #include <iostream>
 #include <fstream>
-#include <syslog .h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
 using namespace std;
 
 const string Programroot = "/usr/geobackup/";
+const string DaemonPath = Programroot+"bin/daemon";
+
+//bool isInstalled(){
+//	if (std::ifstream DaemonPath.c_str())
+//	{
+//     	return true;
+//	}
+//	std::ofstream file(DaemonPath.c_str()));
+//	if (!file)
+//	{
+//    	 return false;
+//	}
+//}
+
+void process(){
+	syslog(LOG_INFO, "Running...");
+}
 
 int main(){
-
 	setlogmask (LOG_UPTO (LOG_NOTICE));
     openlog ("geobackup", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
-  	if(isInstalled()){
-  		
-    	syslog(LOG_INFO, "Entering Daemon");
+  	if(true){
 
-    	pid_t pid, sid;
+    	syslog(LOG_INFO, "Entering Daemon");
+    	//pid_t pid, sid;
 
    		//Fork the Parent Process
-    	pid = fork();
+    	///pid = fork();
 
-    	if (pid < 0) { exit(EXIT_FAILURE); }
+    	///if (pid < 0) { exit(EXIT_FAILURE); }
 
    		//We got a good pid, Close the Parent Process
-    	if (pid > 0) { exit(EXIT_SUCCESS); }
+    	///if (pid > 0) { exit(EXIT_SUCCESS); }
 
     	//Change File Mask
-    	umask(0);
+    	///umask(0);
 
    		 //Create a new Signature Id for our child
-    	sid = setsid();
-    	if (sid < 0) { exit(EXIT_FAILURE); }
+    	///sid = setsid();
+    	///if (sid < 0) { exit(EXIT_FAILURE); }
 
     	//Change Directory
     	//If we cant find the directory we exit with failure.
-    	if ((chdir(Programroot)) < 0) { exit(EXIT_FAILURE); }
-
+    	if ((chdir(Programroot.c_str())) < 0) { exit(EXIT_FAILURE); }
+    	//cout << "work2";
    		 //Close Standard File Descriptors
-   		close(STDIN_FILENO);
-    	close(STDOUT_FILENO);
-   		close(STDERR_FILENO);
+   		//close(STDIN_FILENO);
+    	//close(STDOUT_FILENO);
+   		//close(STDERR_FILENO);
 
     	//----------------
     	//Main Process
     	//----------------
+    	cout << "work3";
     	int x = 0;
     	while(x<5){
     		x++;
         	process();    //Run our Process
         	sleep(60);    //Sleep for 60 seconds
-        	if(x === 5){
+        	if(x == 5){
         		return 0;
         	}
     	}
@@ -60,20 +85,4 @@ int main(){
   		syslog(LOG_WARNING, "Daemon executable not found!");
   		return 0;
   	}
-}
-
-bool isInstalled(){
-	if (std::ifstream(Programroot+"bin/daemon"))
-	{
-     	return true;
-	}
-	std::ofstream file(Programroot+"bin/daemon");
-	if (!file)
-	{
-    	 return false;
-	}
-}
-
-void process(){
-	syslog(LOG_INFO, "Running...");
 }

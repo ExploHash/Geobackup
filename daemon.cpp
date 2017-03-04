3ed8242e29cad662112e665a64e8a8b1712f022b
ExploHash <jbtwaalf@gmail.com>
2017-03-04 09:46:21 +0100

    Changed logging from syslog to simple selfwritten, it works now

--- daemon.cpp
+++ daemon.cpp
@@ -28,61 +28,61 @@ const string DaemonPath = Programroot+"bin/daemon";
 //	}
 //}
 
+int logN(string message){
+  ofstream myfile;
+  myfile.open("/root/build/debug.log", ios::app);
+  myfile << message+"\n";
+  myfile.close();
+  return 0;
+}
+
 void process(){
-	syslog(LOG_INFO, "Running...");
+  logN("running..");
 }
 
 int main(){
-	setlogmask (LOG_UPTO (LOG_NOTICE));
-    openlog ("geobackup", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-
   	if(true){
 
-    	syslog(LOG_INFO, "Entering Daemon");
-    	//pid_t pid, sid;
+    	logN("Entering Daemon");
+    	pid_t pid, sid;
 
    		//Fork the Parent Process
-    	///pid = fork();
+    	pid = fork();
 
-    	///if (pid < 0) { exit(EXIT_FAILURE); }
+    	if (pid < 0) { exit(EXIT_FAILURE); }
 
    		//We got a good pid, Close the Parent Process
-    	///if (pid > 0) { exit(EXIT_SUCCESS); }
+    	if (pid > 0) { exit(EXIT_SUCCESS); }
 
     	//Change File Mask
-    	///umask(0);
+    	umask(0);
 
    		 //Create a new Signature Id for our child
-    	///sid = setsid();
-    	///if (sid < 0) { exit(EXIT_FAILURE); }
+    	sid = setsid();
+    	if (sid < 0) { exit(EXIT_FAILURE); }
 
     	//Change Directory
     	//If we cant find the directory we exit with failure.
-    	if ((chdir(Programroot.c_str())) < 0) { exit(EXIT_FAILURE); }
-    	//cout << "work2";
+    	//if ((chdir(Programroot.c_str())) < 0) { exit(EXIT_FAILURE); }
    		 //Close Standard File Descriptors
-   		//close(STDIN_FILENO);
-    	//close(STDOUT_FILENO);
-   		//close(STDERR_FILENO);
+   		close(STDIN_FILENO);
+    	close(STDOUT_FILENO);
+   		close(STDERR_FILENO);
 
     	//----------------
     	//Main Process
     	//----------------
-    	cout << "work3";
     	int x = 0;
     	while(x<5){
     		x++;
         	process();    //Run our Process
-        	sleep(60);    //Sleep for 60 seconds
+        	sleep(1);    //Sleep for 60 seconds
         	if(x == 5){
         		return 0;
         	}
     	}
-
-    	//Close the log
-    	closelog ();
   	}else{
-  		syslog(LOG_WARNING, "Daemon executable not found!");
+  		logN("Daemon executable not found!");
   		return 0;
   	}
 }

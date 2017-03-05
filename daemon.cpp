0c5952925e85a7b110c943809f5d2de33eb0f0cb
ExploHash <jbtwaalf@gmail.com>
2017-03-05 11:03:23 +0100

    Added parsing function for the Times file Added exec, and gettimesfile function Added loop in process to loop through timesfile and parse every line Added '$' to markup so you can choose to backup always

--- daemon.cpp
+++ daemon.cpp
@@ -12,31 +12,32 @@
 #include <string.h>
 #include <ctime>
 #include <sstream>
-
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
 //const string Programroot = "/usr/geobackup/";
-const string Programroot = "/root/build/geobackup/";
-const string DaemonPath = Programroot+"bin/daemon";
-const string TimesPath = Programroot+"times.txt";
-const string LogPath = "/root/build/debug.log";
+std::const string Programroot = "/root/build/geobackup/";
+std::const string DaemonPath = Programroot+"bin/daemon";
+std::const string TimesPath = Programroot+"times.txt";
+std::const string LogPath = "/root/build/debug.log";
 
-bool isInstalled(){//check if daemon executable is fund
+bool isInstalled(){//check if daemon executable is found
   struct stat buffer;   
   return (stat (DaemonPath.c_str(), &buffer) == 0);
 }
 
 string to_s(int a){//int to string
-stringstream ss;
-ss << a;
-string str = ss.str();
+stringstream ss; //create string stream
+ss << a; //print int to stream
+string str = ss.str(); //convert stream to string
  return str;
 }
 
 int logN(string message){//log message to log file
   ofstream myfile;
-  myfile.open(LogPath.c_str(), ios::app);
+  myfile.open(LogPath.c_str(), ios::app); //openfile to write to end
   myfile << message+"\n";
   myfile.close();
   return 0;
@@ -44,9 +45,12 @@ int logN(string message){//log message to log file
 string getTime(){
   time_t t = time(0);   // get time now
   struct tm * now = localtime( & t ); //set object tm to now
+  int year = now->tm_year + 1900;
+  int month = now->tm_mon + 1;
+  int day = now->tm_mday;
   int hour = now->tm_hour;
   int minute = now->tm_min;
-  return to_s(hour) + ":" + to_s(minute);
+  return to_s(year) + ":" + to_s(month) + ":" + to_s(day) + ":" + to_s(hour) + ":" + to_s(minute);
 }
 
 int gettimesfile_amount(){//get amount of lines of the times.txt
@@ -57,15 +61,105 @@ int gettimesfile_amount(){//get amount of lines of the times.txt
     while (std::getline(myfile, line)){
         ++number_of_lines;
       } 
-    return number_of_lines;
+	return number_of_lines;
+}
+
+string gettimesfile(int a){
+	ifstream file(TimesPath.c_str());
+
+        string myArray[a];
+
+        for(int i = 0; i < 5; ++i)
+        {
+            file >> myArray[i];
+        }
+		return myArray;
+}
+
+string exec(const char* cmd) {//I know, I know, grabbed from StackOverflow :/
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+bool checkTimes(string line, int i){
+	//check if line is comment or empty
+	if("#" == line.substr(0,1) && line.empty()){
+		return false;
+	}
+	//find first space
+	location = strchr(line, ' '); //get location of the first space
+	if(location != NULL){
+		location2 = line.find_first_of(":", location); //get location of the first ':' after the space 
+		if(location2 == string::npos){//checking if space is found in time area
+			//check if time area is correct formatted
+			int strlen = line.length();
+			char lineChars[strlen];
+			strncpy(lineChars, line.c_str(), sizeof(lineChars)); //convert string to chars
+			lineChars[sizeof(lineChars) - 1] = 0;
+			int totalis = 0;
+			for(a = 0; a < location; a = a + 1 ){//loop through chars
+				if(lineChars[a] == ':'){//new ':' found
+					totalis++;
+				}
+				if(strchr(":0123456789/,*!", lineChars[a]) == NULL){//check for know chars
+					logN("Incorrect formatting, illegal token '"+lineChars[a]+"' found on "+TimesPath+"["+to_s(i)+"]. Skipping line.");
+					return false;
+				}
+				//stoped working here
+				//
+				//
+				//
+			}
+			if(totalis < 4){//to little ':'
+				logN("Incorrect formatting, to little ':' on "+TimesPath+"["+to_s(i)+"]. Skipping line.");
+				return false;
+			}
+
+			string currentTime = getTime(); //get current time in year:month:day:hour:minute format
+			int startPos = 0
+			if(line.substr(0,1) == "$"){//check if always backup ('$') is set
+				startPos++;
+			}
+			string Timestime = line.substr(startPos, location-1); //get time section from line
+
+		}else{
+			logN("Space(s) found on "+TimesPath+"["+to_s(i)+"] in the area where the time is defined. Skipping line.");
+			return false;
+		}
+	}else{
+		logN("No spaces found on "+TimesPath+"["+to_s(i)+"]. Skipping line.");
+		return false;
+	}
 }
 
 void process(){//the process which get runned every minute
   logN("Running process...");
-  //check if /usr/geobackup/times.txt exists
+  struct stat buffer;   
+  if(stat(TimesPath.c_str(), &buffer) == 0){
   int line_amount = gettimesfile_amount();
-  //logN(to_s(line_amount));
-}
+  string lines[line_amount] = gettimesfile(line_amount);
+  for(int i = 0; i < line_amount; ++i){
+	  bool MustRunNow = checkTimes(lines[i], i);
+	  if(MustRunNow){
+		  exec(generateCommand("backup"));
+	  }
+  }
+}else{
+	logN("Times file (still) not accessable!");
+	return false;
+}}
 
 int main(){
   	if(isInstalled()){
